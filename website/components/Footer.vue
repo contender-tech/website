f896ebf8b680ab5d55b01edaebc0c8b2310aacd4
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-10 02:12:39 +0100

    Create home page components

---
+++ website/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="bg-white">
+    <div
+      class="container mx-auto px-6 py-12 text-secondary-500 text-center border-t border-gray-300"
+    >
+      <p>Copyright &copy; 2021 Contender Tech Ltd. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
