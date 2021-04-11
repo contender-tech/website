c8e8f73c29608aa4ea92f8d2dc323224d852f6e9
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-11 23:10:11 +0100

    Refresh home components

--- website/components/HomeHero.vue
+++ website/components/HomeHero.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="relative overflow-x-hidden px-6 pb-6">
-    <img src="~/assets/wave.svg" class="absolute top-0 left-40" />
-    <div class="container mx-auto relative">
-      <div class="flex flex-col md:flex-row items-center pt-32 pb-16 md:pb-0">
-        <div class="md:w-1/2 lg:w-1/3 mb-4 sm:mb-16 md:mb-0">
-          <h2 class="text-xl font-bold text-secondary-600 uppercase mb-2">
-            Contender Tech Ltd.
-          </h2>
-          <h1
-            class="text-4xl md:text-5xl font-bold text-secondary leading-tight mb-6 md:mb-10"
-          >
-            Logistics solutions, powered by AI
-          </h1>
-          <a
-            href="#our-process"
-            class="bg-primary px-6 md:px-8 py-3 md:py-4 text-lg md:text-xl text-white font-bold uppercase rounded hover:bg-primary-400"
-            >More info</a
-          >
+  <div id="hero">
+    <div id="background" class="relative">
+      <img class="absolute top-0 lg_left-0 right-0 z-0 w-full" />
+    </div>
+    <div
+      id="content"
+      class="flex flex-col sm:flex-row items-center container lg:max-w-5xl relative top-0 left-0 right-0 px-8 py-4 mx-auto mb-8 sm:mb-12 md:mb-16"
+    >
+      <div class="flex flex-1 flex-col mt-40 sm:mb-32">
+        <p class="text-2xl text-gray-50 font-semibold tracking-wide">
+          Contender Tech Ltd.
+        </p>
+        <p class="text-4xl text-gray-50 font-semibold tracking-wide mt-5">
+          AI-Driven Logistics and Supply Chain
+        </p>
+        <div class="flex-col">
+          <a href="#">
+            <button
+              class="flex text-xl text-gray-50 font-semibold tracking-wide bg-blue-500 hover:bg-blue-400 rounded shadow px-5 py-2 mt-8 justify-center"
+            >
+              Learn more
+            </button>
+          </a>
         </div>
-        <div class="mt-16 sm:mt-0 flex-1 flex justify-end">
-          <img src="~/assets/all-the-data.svg" />
-        </div>
+      </div>
+      <div class="flex-1 mt-4 sm:mt-40 mb-4 sm:mb-32">
+        <img src="https://svgshare.com/i/W4W.svg" />
       </div>
     </div>
   </div>
