a8051f869c624b7ab36d08a30fc7e6371ab79a4f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-12 23:44:09 +0100

    Create platform section

---
+++ website/components/HomePlatform.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-gray-50 overflow-hidden">
+    <div
+      class="flex flex-col items-center container lg:max-w-5xl relative top-0 left-0 right-0 px-4 sm:px-6 py-8 mx-auto"
+    >
+      <div class="flex flex-col max-w-3xl mt-5 mb-8 sm:mb-16">
+        <h2 class="flex text-3xl text-gray-900 text-center mx-auto">
+          Our platform
+        </h2>
+        <div class="sm:hidden mt-5 mx-auto">
+          <img
+            class="rounded-md shadow-md"
+            src="~/assets/app-screenshot.png"
+            width="383"
+            height="202"
+            alt="A delivered stack of boxes"
+            style="width: 383px"
+          />
+        </div>
+        <p class="flex text-lg text-gray-700 mt-6">
+          Contender is a data platform for managing your inventory. With a
+          built-in layer of AI capabilities, our one-click demand forecasting
+          makes it easy to inform buying decisions.
+        </p>
+        <div class="hidden sm:inline invisible" style="height: 404px"></div>
+        <div
+          class="absolute hidden sm:inline rounded-md shadow-md overflow-hidden mt-40 md:mt-36"
+        >
+          <img
+            src="~/assets/app-screenshot.png"
+            width="766"
+            height="404"
+            alt="A delivered stack of boxes"
+            style="max-width: none; width: 766px"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
