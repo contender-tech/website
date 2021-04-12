7606cb3f25e51cfff0a325bb9a79958bcd258714
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-12 23:44:15 +0100

    Create features section

---
+++ website/components/HomeFeatures.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="bg-gray-800 overflow-hidden">
+    <div
+      class="flex flex-col items-center container lg:max-w-5xl relative top-0 left-0 right-0 px-4 sm:px-6 py-8 mb-8 mx-auto"
+    >
+      <div class="flex flex-col mt-5">
+        <h2 class="flex text-3xl text-gray-300 text-center">Features</h2>
+        <p class="flex text-lg text-gray-200 mt-6">
+          Contender is a data platform for managing your inventory. With a layer
+          of AI capabilities built on top, our one-click demand forecasting
+          makes it easy to inform buying decisions.
+        </p>
+      </div>
+      <div class="flex flex-col md:flex-row md:mt-5 space-x-0 md:space-x-16">
+        <div class="md:flex-1 mt-4 md:mt-3">
+          <div class="text-blue-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+              />
+            </svg>
+          </div>
+          <div class="text-lg mt-3 font-semibold text-gray-300">
+            Free to try
+          </div>
+          <p class="mt-1 text-gray-400">
+            Get started instantly with a basic plan. Manage your inventory and
+            work with up to 5 teammates.
+          </p>
+        </div>
+        <div class="md:flex-1 mt-4 md:mt-3">
+          <div class="text-blue-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div class="text-lg mt-3 font-semibold text-gray-300">
+            CSV import + export
+          </div>
+          <p class="mt-1 text-gray-400">
+            Not quite ready to ditch Excel? Maintain the power of your legacy
+            system with our easy spreadsheet handling.
+          </p>
+        </div>
+        <div class="md:flex-1 mt-4 md:mt-3">
+          <div class="text-blue-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              />
+            </svg>
+          </div>
+          <div class="text-lg mt-3 font-semibold text-gray-300">
+            Full auditing
+          </div>
+          <p class="mt-1 text-gray-400">
+            Powered by SQL, our smart database gives you full inventory auditing
+            down to the per-action level.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
