67c19a657e5cdc5e013c332ee9340d828e2279a1
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-12 23:52:44 +0100

    Add placeholder for mobile nav toggle

--- website/components/Navbar.vue
+++ website/components/Navbar.vue
@@ -33,9 +33,22 @@
       <li>
         <a class="flex-none" href="#">
           <p
-            class="text-gray-50 font-semibold tracking-wide bg-blue-100 bg-opacity-30 hover:bg-opacity-40 rounded px-7 py-2"
+            class="text-gray-200 font-semibold tracking-wide hover:text-gray-50 px-2 py-2"
           >
-            mob
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
           </p>
         </a>
       </li>
