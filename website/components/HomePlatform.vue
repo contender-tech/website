8e01537b2bcacbaac989f8e2d62b7379be69dc3a
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-13 02:03:26 +0100

    Add background shapes

--- website/components/HomePlatform.vue
+++ website/components/HomePlatform.vue
@@ -4,6 +4,48 @@
       class="flex flex-col items-center container xl:max-w-6xl relative top-0 left-0 right-0 px-4 sm:px-6 py-10 mx-auto"
     >
       <div class="flex flex-col max-w-3xl mt-12 mb-8 sm:mb-24">
+        <div
+          class="absolute left-0 top-80 py-24"
+          style="transform: translateX(-100%)"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="250"
+            height="250"
+            viewBox="0 0 250 250"
+          >
+            <title>Metaball</title>
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Layer_1-2" data-name="Layer 1">
+                <path
+                  d="M29.06653,10.24905c40.28883-9.97,115.45675-21.87716,143.77,13.59655,18.6879,23.41407,33.407,51.05792,49.748,76.03276,13.675,20.90027,38.51108,47.48113,21.87454,73.36669-13.60354,21.16636-51.10912,34.36848-72.15891,46.55423-26.682,15.44625-53.32844,35.73913-86.45,28.79906-36.86438-7.72432-53.50214-38.159-61.19995-70.82181C17.30022,146.5896-29.48595,29.32854,29.06653,10.24905Z"
+                  style="fill: #eff3f8"
+                />
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div
+          class="absolute right-0 top-0 py-24"
+          style="transform: translateX(100%)"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="250"
+            height="250"
+            viewBox="0 0 250 250"
+          >
+            <title>Metaball</title>
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Layer_1-2" data-name="Layer 1">
+                <path
+                  d="M29.06653,10.24905c40.28883-9.97,115.45675-21.87716,143.77,13.59655,18.6879,23.41407,33.407,51.05792,49.748,76.03276,13.675,20.90027,38.51108,47.48113,21.87454,73.36669-13.60354,21.16636-51.10912,34.36848-72.15891,46.55423-26.682,15.44625-53.32844,35.73913-86.45,28.79906-36.86438-7.72432-53.50214-38.159-61.19995-70.82181C17.30022,146.5896-29.48595,29.32854,29.06653,10.24905Z"
+                  style="fill: #eff3f8"
+                />
+              </g>
+            </g>
+          </svg>
+        </div>
         <h2 class="flex text-3xl text-gray-900 text-center mx-auto">
           Our platform
         </h2>
