4a14c5a000fbfe3fb34336d4624dafc784314082
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-14 02:25:35 +0100

    Refresh navbar

--- website/components/Navbar.vue
+++ website/components/Navbar.vue
@@ -1,59 +1,76 @@
 <template>
-  <nav
-    class="flex flex-row container xl:max-w-6xl absolute top-0 left-0 right-0 z-10 px-4 sm:px-6 py-4 sm:py-6 mx-auto"
-  >
-    <ul class="flex flex-grow items-center py-1 mx-auto">
-      <li>
-        <a href="#" class="flex items-center space-x-3">
-          <img
-            class="w-10 filter brightness-125"
-            src="~/assets/logo-80x96.png?webp"
-            width="40"
-            height="48"
-            alt="The Contender Tech logo, a blue whale spinning in a circle"
-          />
-          <p class="text-2xl text-gray-50 font-semibold tracking-wide">
-            Contender
-          </p>
-        </a>
-      </li>
-    </ul>
-    <ul class="hidden sm:flex items-center space-x-2 md:space-x-2 py-2 mx-auto">
-      <li>
-        <a class="flex-none" href="#">
-          <p
-            class="text-gray-50 font-semibold tracking-wide bg-blue-400 bg-opacity-30 hover:bg-opacity-40 rounded px-5 py-2"
+  <div class="fixed z-10 w-full bg-white shadow-md mx-auto">
+    <nav class="flex container xl:max-w-6xl px-5 py-4 mx-auto">
+      <ul class="flex flex-grow space-x-4">
+        <li>
+          <a
+            href="#"
+            class="flex items-center space-x-1 text-gray-700 duration-200 hover:text-blue-500 md:mr-5"
           >
-            Home
-          </p>
-        </a>
-      </li>
-    </ul>
-    <ul class="flex sm:hidden items-center space-x-2 md:space-x-2 py-2 mx-auto">
-      <li>
-        <a class="flex-none" href="#">
-          <p
-            class="text-gray-200 font-semibold tracking-wide hover:text-gray-50 px-2 py-2"
+            <img
+              src="~/assets/logo-96x96.png"
+              class="w-12 filter saturate-150"
+            />
+            <span class="text-xl font-semibold tracking-tight">Contender</span>
+          </a>
+        </li>
+        <li class="hidden sm:flex items-center">
+          <a
+            href="#"
+            class="text-gray-700 font-light duration-200 hover:bg-gray-100 rounded px-3 py-1"
+            >Product</a
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        </li>
+        <li class="hidden sm:flex items-center">
+          <a
+            href="#"
+            class="text-gray-700 font-light duration-200 hover:bg-gray-100 rounded px-3 py-1"
+            >Pricing</a
+          >
+        </li>
+        <li class="hidden sm:flex items-center">
+          <a
+            href="#"
+            class="text-gray-700 font-light duration-200 hover:bg-gray-100 rounded px-3 py-1"
+            >Contact us</a
+          >
+        </li>
+      </ul>
+      <ul class="hidden sm:flex flex-none items-center space-x-8">
+        <li>
+          <a
+            href="#"
+            class="text-white bg-blue-500 duration-200 hover:bg-blue-400 rounded shadow-sm px-5 py-3"
+          >
+            Try for free
+          </a>
+        </li>
+      </ul>
+      <ul class="flex sm:hidden flex-none items-center space-x-8">
+        <li>
+          <a href="#" class="flex-none"
+            ><p
+              class="text-gray-800 font-semibold tracking-wide hover:bg-gray-100 rounded px-2 py-2"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </p>
-        </a>
-      </li>
-    </ul>
-  </nav>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="h-6 w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg></p
+          ></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
