fb5cece3e2f083c435633e7318e2afa7a671887b
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-14 02:26:11 +0100

    Add attribution to footer

--- website/components/Footer.vue
+++ website/components/Footer.vue
@@ -1,45 +1,52 @@
 <template>
   <footer class="bg-gray-800 overflow-hidden">
-    <div
-      class="flex relative flex-col sm:flex-row container xl:max-w-6xl items-center sm:justify-between px-4 sm:px-6 py-5 mx-auto"
-    >
-      <ul class="flex my-1">
-        <li>
-          <div class="text-gray-100 sm:text-sm font-semibold">
-            Contender Tech Ltd. © 2021
-          </div>
-        </li>
-      </ul>
-      <ul class="flex space-x-4 my-1">
-        <li>
-          <a
-            href="https://github.com/contender-tech/"
-            class="text-white hover:underline sm:text-sm"
-            >GitHub</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/company/contender-tech/"
-            class="text-white hover:underline sm:text-sm"
-            >LinkedIn</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/contendertech/"
-            class="text-white hover:underline sm:text-sm"
-            >Instagram</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/ContenderTech"
-            class="text-white hover:underline sm:text-sm"
-            >Twitter</a
-          >
-        </li>
-      </ul>
+    <div class="flex flex-col container xl:max-w-6xl px-5 mt-4 mb-12 mx-auto">
+      <div
+        class="flex flex-col sm:flex-row items-center sm:justify-between py-5"
+      >
+        <ul class="flex my-1">
+          <li>
+            <div class="text-gray-100 sm:text-sm font-semibold">
+              Contender Tech Ltd. © 2021
+            </div>
+          </li>
+        </ul>
+        <ul class="flex space-x-4 my-1">
+          <li>
+            <a
+              href="https://github.com/contender-tech/"
+              class="text-white hover:underline sm:text-sm"
+              >GitHub</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/company/contender-tech/"
+              class="text-white hover:underline sm:text-sm"
+              >LinkedIn</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/contendertech/"
+              class="text-white hover:underline sm:text-sm"
+              >Instagram</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/ContenderTech"
+              class="text-white hover:underline sm:text-sm"
+              >Twitter</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="text-sm text-gray-400">
+        <a href="http://www.freepik.com" class="hover:underline"
+          >Server graphic designed by fullvector / Freepik</a
+        >
+      </div>
     </div>
   </footer>
 </template>
