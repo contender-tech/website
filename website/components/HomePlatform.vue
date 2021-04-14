c4d1d16485d7f05eec1f62ba4f10addc119332bc
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-14 02:25:39 +0100

    Refresh platform

--- website/components/HomePlatform.vue
+++ website/components/HomePlatform.vue
@@ -1,73 +1,21 @@
 <template>
   <div class="bg-gray-50 overflow-hidden">
     <div
-      class="flex flex-col items-center container xl:max-w-6xl relative top-0 left-0 right-0 px-4 sm:px-6 py-10 mx-auto"
+      class="flex flex-col items-center container xl:max-w-6xl relative top-0 left-0 right-0 px-5 mx-auto"
     >
-      <div class="flex flex-col max-w-3xl mt-12 mb-8 sm:mb-24">
-        <div
-          class="absolute left-0 top-80 py-24"
-          style="transform: translateX(-100%)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="250"
-            height="250"
-            viewBox="0 0 250 250"
-          >
-            <title>Metaball</title>
-            <g id="Layer_2" data-name="Layer 2">
-              <g id="Layer_1-2" data-name="Layer 1">
-                <path
-                  d="M29.06653,10.24905c40.28883-9.97,115.45675-21.87716,143.77,13.59655,18.6879,23.41407,33.407,51.05792,49.748,76.03276,13.675,20.90027,38.51108,47.48113,21.87454,73.36669-13.60354,21.16636-51.10912,34.36848-72.15891,46.55423-26.682,15.44625-53.32844,35.73913-86.45,28.79906-36.86438-7.72432-53.50214-38.159-61.19995-70.82181C17.30022,146.5896-29.48595,29.32854,29.06653,10.24905Z"
-                  style="fill: #eff3f8"
-                />
-              </g>
-            </g>
-          </svg>
-        </div>
-        <div
-          class="absolute right-0 top-0 py-24"
-          style="transform: translateX(100%)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="250"
-            height="250"
-            viewBox="0 0 250 250"
-          >
-            <title>Metaball</title>
-            <g id="Layer_2" data-name="Layer 2">
-              <g id="Layer_1-2" data-name="Layer 1">
-                <path
-                  d="M29.06653,10.24905c40.28883-9.97,115.45675-21.87716,143.77,13.59655,18.6879,23.41407,33.407,51.05792,49.748,76.03276,13.675,20.90027,38.51108,47.48113,21.87454,73.36669-13.60354,21.16636-51.10912,34.36848-72.15891,46.55423-26.682,15.44625-53.32844,35.73913-86.45,28.79906-36.86438-7.72432-53.50214-38.159-61.19995-70.82181C17.30022,146.5896-29.48595,29.32854,29.06653,10.24905Z"
-                  style="fill: #eff3f8"
-                />
-              </g>
-            </g>
-          </svg>
-        </div>
-        <h2 class="flex text-3xl text-gray-900 text-center mx-auto">
-          Our platform
-        </h2>
-        <div class="sm:hidden mt-8 mx-auto">
-          <img
-            class="rounded-md shadow-md"
-            src="~/assets/app-screenshot.png?webp"
-            width="383"
-            height="202"
-            alt="An in-app screenshot of Contender, an inventory management platform"
-            style="width: 383px"
-          />
-        </div>
-        <p class="flex text-lg text-gray-700 mt-8">
-          Contender is a data platform for managing your inventory. With a
-          built-in layer of AI capabilities, our one-click demand forecasting
-          makes it easy to inform buying decisions.
+      <div class="flex flex-col max-w-3xl mt-12 mb-24">
+        <h2 class="text-3xl text-gray-900 text-center mx-auto">Our platform</h2>
+        <p class="text-lg text-gray-700 mt-8">
+          Contender is a data platform for managing
+          <span class="hidden sm:inline"> your </span>
+          inventory.
         </p>
-        <div class="hidden sm:inline invisible" style="height: 404px"></div>
-        <div
-          class="absolute hidden sm:inline rounded-md shadow-md overflow-hidden mt-48 md:mt-40"
-        >
+        <p class="text-lg text-gray-700 mt-3">
+          With a built-in layer of artificial intelligence, our one-click demand
+          forecasting makes buying decisions easier.
+        </p>
+        <div class="invisible" style="height: 404px"></div>
+        <div class="absolute rounded-md shadow-md overflow-hidden mt-48">
           <img
             src="~/assets/app-screenshot.png?webp"
             width="766"
